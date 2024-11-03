abad12bb87bc056f67221c4668c0afaa110a07bb
Cisa <galuhsaputra050508@gmail.com>
2024-11-03 12:54:12 +0700

    Player Input - Ep.14

--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -31,9 +31,7 @@ export class BattleScene extends Phaser.Scene {
         this.add.image(256, 316, MONSTER_ASSET_KEYS.IGUANIGNITE, 0).setFlipX(true);
 
         //render out the player health bar
-        const playerMonsterName = this.add.text(
-            30, 
-            20, 
+        const playerMonsterName = this.add.text(30, 20, 
             MONSTER_ASSET_KEYS.IGUANIGNITE, 
             {
                 color: '#7E3D3F',
@@ -41,9 +39,7 @@ export class BattleScene extends Phaser.Scene {
             }
         );
         this.add.container(556, 318, [
-            this.add
-                .image(0,0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
-                .setOrigin(0),
+            this.add.image(0,0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND).setOrigin(0),
             playerMonsterName,
             this.#createHealthBar(34, 34),
             this.add.text(playerMonsterName.width + 35, 23, 'L5', {
@@ -64,9 +60,7 @@ export class BattleScene extends Phaser.Scene {
         ]);
 
         //render out the enemy health bar
-        const enemyMonsterName = this.add.text(
-            30, 
-            20, 
+        const enemyMonsterName = this.add.text(30, 20, 
             MONSTER_ASSET_KEYS.CARNODUSK, 
             {
                 color: '#7E3D3F',
@@ -74,10 +68,7 @@ export class BattleScene extends Phaser.Scene {
             }
         );
         this.add.container(0, 0, [
-            this.add
-                .image(0,0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
-                .setOrigin(0)
-                .setScale(1, 0.8),
+            this.add.image(0,0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND).setOrigin(0).setScale(1, 0.8),
             enemyMonsterName,
             this.#createHealthBar(34, 34),
             this.add.text(enemyMonsterName.width + 35, 23, 'L5', {
@@ -135,19 +126,10 @@ export class BattleScene extends Phaser.Scene {
      */
     #createHealthBar(x, y) {
         const scaleY = 0.7;
-        const leftCap = this.add
-            .image(x, y,HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
-            .setOrigin(0, 0.5)
-            .setScale(1, scaleY);
-        const middle = this.add
-            .image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
-            .setOrigin(0, 0.5)
-            .setScale(1, scaleY);
+        const leftCap = this.add.image(x, y,HEALTH_BAR_ASSET_KEYS.LEFT_CAP).setOrigin(0, 0.5).setScale(1, scaleY);
+        const middle = this.add.image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE).setOrigin(0, 0.5).setScale(1, scaleY);
         middle.displayWidth = 360;
-        const rightCap = this.add
-            .image(middle.x + middle.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
-            .setOrigin(0, 0.5)
-            .setScale(1, scaleY);
+        const rightCap = this.add.image(middle.x + middle.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP).setOrigin(0, 0.5).setScale(1, scaleY);
 
         return this.add.container(x, y, [leftCap, middle, rightCap]);
     }
