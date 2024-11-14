36a9e8fad2f2d7f94bf7da94c724844ede7d07d5
Cisa <galuhsaputra050508@gmail.com>
2024-11-14 09:07:00 +0700

    player collision added

--- src/scenes/world-scene.js
+++ src/scenes/world-scene.js
@@ -4,7 +4,7 @@ import { SCENE_KEYS } from "./scene-keys.js"
 import { Player } from '../world/characters/player.js'
 import { Controls } from '../utils/controls.js'
 import { DIRECTION } from '../common/direction.js'
-import { TILE_SIZE } from '../config.js'
+import { TILED_COLLISION_LAYER_ALPHA, TILE_SIZE } from '../config.js'
 
 /** @type {import('../types/typedef.js').Coordinate} */
 const PLAYER_POSITION = Object.freeze({
@@ -29,9 +29,26 @@ export class WorldScene extends Phaser.Scene {
 
         const x = 6 * TILE_SIZE
         const y = 22 * TILE_SIZE
+
+        // this value comes from the width of the level background image we are using
+        // we set the max camera width to the size of our image in order to control what
+        // is visible to the player, since the phaser game world is infinite.
         this.cameras.main.setBounds(0, 0, 1280, 2176)
         this.cameras.main.setZoom(0.8)
         this.cameras.main.centerOn(x, y)
+
+        const map = this.make.tilemap({ key: WORLD_ASSET_KEYS.WORLD_MAIN_LEVEL })
+        const collisionTiles = map.addTilesetImage('collision', WORLD_ASSET_KEYS.WORLD_COLLISION)
+        if (!collisionTiles) {
+            console.log(`[${WorldScene.name}:create] encountered error while creating collision tileset using data from tiled`)
+            return
+        }
+        const collisionLayer = map.createLayer('Collision', collisionTiles, 0, 0)
+        if (!collisionLayer) {
+            console.log(`[${WorldScene.name}:create] encountered error while creating collision layer using data from tiled`)
+            return
+        }
+        collisionLayer.setAlpha(TILED_COLLISION_LAYER_ALPHA).setDepth(2)
 
         this.add.image(0, 0, WORLD_ASSET_KEYS.WORLD_BACKGROUND, 0).setOrigin(0)
 
@@ -39,8 +56,11 @@ export class WorldScene extends Phaser.Scene {
             scene: this,
             position: PLAYER_POSITION,
             direction: DIRECTION.DOWN,
+            collisionLayer: collisionLayer,
         })
         this.cameras.main.startFollow(this.#player.sprite)
+
+        this.add.image(0, 0, WORLD_ASSET_KEYS.WORLD_FOREGROUND, 0).setOrigin(0)
 
         this.#controls = new Controls(this)
 
